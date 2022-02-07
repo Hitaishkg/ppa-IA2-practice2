1089a568e051b54497f8aec072dd6692c89b5909
hitaish kg <91547035+Hitaishkg@users.noreply.github.com>
2022-02-07 09:23:01 +0000

    p2 done

---
+++ p2original.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int input_side(){
+    int a;
+    printf("Enter the length of side 1\n");
+    scanf("%d",&a);
+    return a;
+}
+int check_scalene(int a, int b, int c){
+    int isscalene;
+    if(a==b || b==c ||c==a){
+        isscalene=-1;
+    }
+    else if(a!=b &&b!=c&&c!=a){
+        isscalene=1;
+    }
+    return isscalene;
+}
+void output(int a, int b, int c, int isscalene){
+    if(isscalene==-1){
+    printf("The triangle of sides %d %d %d is not scalene",a,b,c);
+    }
+    else if(isscalene==1){
+        printf("the triangle of sides %d %d %d is scalene",a,b,c);
+    }
+}
+int main(){
+    int a,b,c,isscalene;
+    a=input_side();
+    b=input_side();
+    c=input_side();
+    isscalene=check_scalene(a,b, c);
+    output(a,b, c,isscalene);
+    return 0;
+    
+}
+
